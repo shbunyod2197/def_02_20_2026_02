83d1e5f08a9c600cd20961a30a664f8d32c7dbc8
Bunyod <shbunyod2197@gmail.com>
2026-02-20 15:46:26 +0500

    Create  main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+# 1
+def salom_berish(ism="Do'st"):
+    return ism
+
+print(f"Salom: {salom_berish()}")
+# 2
+def daraja(a, b):
+    return a ** b
+s = daraja(3, 4)
+print(s)
+3
+def matn_takrorir_son(a, b):
+    return a * b
+
+d = matn_takrorir_son("shhjaJS", 3)
+print(d)
+# 4
+def yosh_ism(a, b):
+    return a, b
+
+a = yosh_ism("bunyod", 18)
+print(a)
+# 5
+def summ(a, b):
+    return a + b
+a = summ(3, 6)
+print(a)
+# 6
+def summ(a, b):
+    return a * b
+a = summ(3, 6)
+print(a)
+# 7
+def xabar(a):
+    return a
+po = xabar("Nagapla")
+print(po)
+# 8
+def raqam_chegara(a, b):
+    return a > b
+
+s = raqam_chegara(10, 9)
+print(s)
+# 9
+def a(a, b):
+    return a + b
+
+s = a("82348742", "hsbdg")
+print(s)
+# 10
+def salom(a):
+    return f"Xushkelibsiz: {a}"
+
+d = salom("Bunyod")
+print(d)
